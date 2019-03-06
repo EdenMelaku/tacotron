fb860f6d1b5b269b3c0eadc0c8c903d3c5e949ef
eden melaku <edenzzzmelaku@gmail.com>
2019-03-06 14:14:43 +0300

    Merge branch 'master' of https://github.com/EdenMelaku/tacotron
    # Conflicts:
    #	README.md
    #	models/tacotron.py
    hparams edited

--- hparams.py
+++ hparams.py
@@ -2,10 +2,13 @@ import tensorflow as tf
 
 
 # Default hyperparameters:
+from text.cleaners import basic_cleaners
+
 hparams = tf.contrib.training.HParams(
   # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
   # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
-  cleaners='english_cleaners',
+  cleaners=basic_cleaners,
+
 
   # Audio:
   num_mels=80,
